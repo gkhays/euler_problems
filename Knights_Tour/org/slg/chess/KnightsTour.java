eb9c7b45a3cda1d76fa8d8dfba3cfcf0dc9dbd3c
Garve Hays <gkhays@gmail.com>
2016-04-08 18:11:28 -0500

    Make Upper Limit Static
    Upper bound should be final and static.

--- Knights_Tour/org/slg/chess/KnightsTour.java
+++ Knights_Tour/org/slg/chess/KnightsTour.java
@@ -4,11 +4,23 @@ import java.awt.Point;
 
 public class KnightsTour {
 
-	private int N = 8;
+	private static final int N = 8;
 	private int[][] chessBoardArray = new int[8][8];
 	private int[] xKnightMoves = { 2, 1, -1, -2, -2, -1, 1, 2 };
 	private int[] yKnightMoves = { 1, 2, 2, 1, -1, -2, -2, -1 };
 	
+	/*
+	 * <pre>
+	 * 0 59 38 33 30 17  8 63
+ 	 * 37 34 31 60  9 62 29 16
+ 	 * 58  1 36 39 32 27 18  7
+	 * 35 48 41 26 61 10 15 28
+	 * 42 57  2 49 40 23  6 19
+	 * 47 50 45 54 25 20 11 14
+ 	 * 56 43 52  3 22 13 24  5
+ 	 * 51 46 55 44 53  4 21 12
+ 	 * </pre>
+	 */
     public static void main(String[] args) {
     	KnightsTour tour = new KnightsTour();
         tour.run();        
